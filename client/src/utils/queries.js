13085f46bca49bd8a7f9d04b907d971f205ed2c7
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-08-01 01:40:38 -0400

    worked on pages and components

---
+++ client/src/utils/queries.js
@@ -0,0 +1,20 @@
+const { gql } = require("@apollo/client");
+
+export const GET_ME = gql`
+  query me {
+    me {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        description
+        title
+        image
+        link
+      }
+    }
+  }
+`;
