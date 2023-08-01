08cda7d8908ac75f6f406ea4b77053821d53c6f7
TimeBytes <123673532+TimeBytes@users.noreply.github.com>
2023-08-01 01:09:37 -0400

    worked on typedefs and resolvers, auth.js and server.js - backend should be working

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
-
+const authMiddleware = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+const db = require("./config/connection");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,8 +15,18 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.use(routes);
+const startApolloServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+startApolloServer();
